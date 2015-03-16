f30639961d723a0fa5ff959258f499e4647c6dc5
Silas Barta <sbarta@gmail.com>
2015-03-15 20:23:56 -0700

    Bind for query.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,11 +10,10 @@ describe('PhoneCat App', function() {
       browser.get('app/index.html');
     });
 
+    var phoneList = element.all(by.repeater('phone in phones'));
+    var query = element(by.model('query'));
 
     it('should filter the phone list as a user types into the search box', function() {
-
-      var phoneList = element.all(by.repeater('phone in phones'));
-      var query = element(by.model('query'));
 
       expect(phoneList.count()).toBe(3);
 
@@ -25,5 +24,14 @@ describe('PhoneCat App', function() {
       query.sendKeys('motorola');
       expect(phoneList.count()).toBe(2);
     });
+
+    it('should display the current filter value in the title bar', function() {
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+      query.sendKeys('nexus');
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    });
+
   });
 });
